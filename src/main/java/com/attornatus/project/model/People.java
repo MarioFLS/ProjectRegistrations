e844047a73442ec3e63da39b99e0062e1878149a
MarioFLS <mariofernandolsantos@gmail.com>
2023-01-03 00:39:53 -0300

    perf: Exclundo import não usado

--- src/main/java/com/attornatus/project/model/People.java
+++ src/main/java/com/attornatus/project/model/People.java
@@ -14,7 +14,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 @Entity
